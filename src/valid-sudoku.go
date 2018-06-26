a6824fe801099cb1f74ab525ffc8bbaf6a6a0fda
Agus <agustinmodugno@hotmail.com>
2018-06-26 20:21:56 -0300

    Finished sudoku validation

--- src/valid-sudoku.go
+++ src/valid-sudoku.go
@@ -1,11 +1,33 @@
 package main
 
 import "fmt"
-// import "strings"
+import "math"
+import "os"
 
 func main(){
-  grid := []string{"53..7....", "6..195...", ".98....6.", "8...6...3", "4..8.3..1", "7...2...6", ".6....28.", "...419..5", "....8..79"}
-  fmt.Println(isValid(grid[0]))
+  rows := []string{"53..7....", "6..195...", ".98....6.", "8...6...3", "4..8.3..1", "7...2...6", ".6....28.", "...419..5", "....8..79"}
+  sizeOfBox := len(rows[0]) // Get amount of cells in each sub-box, will be the same amount of rows/columns
+  sizeOfSubBoxRow := int(math.Sqrt(float64(sizeOfBox))); // Since each sub-box is NxN size
+  boxes := make([]string,sizeOfBox)
+  columns := make([]string,sizeOfBox)
+  for i, row := range rows{
+
+    if !isValid(row) { // Check if row is valid
+      os.Exit(1);
+    }
+    rowValues := []rune(row)
+    for j, rowValue := range rowValues{
+      boxes[((j/sizeOfSubBoxRow)+(sizeOfSubBoxRow*(i/sizeOfSubBoxRow)))] += string(rowValue);
+      columns[j] +=  string(rowValue);
+    }
+  }
+  for _, column := range columns{
+    if !isValid(column){ os.Exit(1); }
+  }
+  for _, box := range boxes{
+    if !isValid(box){ os.Exit(1); }
+  }
+  fmt.Println("Valid sudoku!!!");
 }
 
 func isValid(values string) bool{
@@ -13,11 +35,11 @@ func isValid(values string) bool{
   for _, val := range values{
     if val == '.'{ continue;}
     if valuesMap[val] == true {
+      fmt.Println("Invalid sudoku");
       return false;
-      } else {
-        valuesMap[val] = true
+      }else{
+        valuesMap[val] = true;
       }
     }
-
     return true;
   }
