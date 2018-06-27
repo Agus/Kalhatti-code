1e22fce6426f0430fd577ad5ac468a9118e4b2c4
Agus <agustinmodugno@hotmail.com>
2018-06-26 22:01:27 -0300

    Using outputs of Readme.md

--- src/implement-power-function.py
+++ src/implement-power-function.py
@@ -3,3 +3,5 @@ import math
 class CustomMath:
     def pow(self,x,n,d):
         return abs(math.pow(x,n) % d);
+
+print(pow(2,3,3))
--- src/valid-sudoku.go
+++ src/valid-sudoku.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 import "math"
-import "os"
 
 func main(){
   rows := []string{"53..7....", "6..195...", ".98....6.", "8...6...3", "4..8.3..1", "7...2...6", ".6....28.", "...419..5", "....8..79"}
-  sizeOfBox := len(rows[0]) // Get amount of cells in each sub-box, will be the same amount of rows/columns
-  sizeOfSubBoxRow := int(math.Sqrt(float64(sizeOfBox))); // Since each sub-box is NxN size
+  result := validSudoku(rows);
+  fmt.Println(result);
+}
+
+func validSudoku(rows []string) int{
+  // Get amount of cells in each sub-box, will be the same amount of rows or columns
+  sizeOfBox := len(rows[0])
+  // Since each sub-box is NxN size
+  sizeOfSubBoxRow := int(math.Sqrt(float64(sizeOfBox)));
   boxes := make([]string,sizeOfBox)
   columns := make([]string,sizeOfBox)
   for i, row := range rows{
-
-    if !isValid(row) { // Check if row is valid
-      os.Exit(1);
-    }
+    // Check if row is valid
+    if !isValid(row){ return 0;}
     rowValues := []rune(row)
     for j, rowValue := range rowValues{
       boxes[((j/sizeOfSubBoxRow)+(sizeOfSubBoxRow*(i/sizeOfSubBoxRow)))] += string(rowValue);
@@ -22,12 +26,12 @@ func main(){
     }
   }
   for _, column := range columns{
-    if !isValid(column){ os.Exit(1); }
+    if !isValid(column){ return 0; }
   }
   for _, box := range boxes{
-    if !isValid(box){ os.Exit(1); }
+    if !isValid(box){ return 0; }
   }
-  fmt.Println("Valid sudoku!!!");
+  return 1;
 }
 
 func isValid(values string) bool{
@@ -35,7 +39,6 @@ func isValid(values string) bool{
   for _, val := range values{
     if val == '.'{ continue;}
     if valuesMap[val] == true {
-      fmt.Println("Invalid sudoku");
       return false;
       }else{
         valuesMap[val] = true;
